ac2e8dfce0d084449a7c8308048cbb32567ae350
a <56597359+sllkx@users.noreply.github.com>
2026-02-25 15:12:20 +0900

    Update list_json2.js

--- list_json2.js
+++ list_json2.js
@@ -1,8 +1,8 @@
 {
   "links": [
 	"https://omg10.com/4/10628199",
-	"https://www.effectivegatecpm.com/jxz4jd7r?key=c69dbf238e855fdc108af717c6363e6b",
-    "https://impracticaltap.com/S0xXKd",
-	"https://mploejuiashsatea.com?DivzC=1244116"
+	"http://isai.kr",
+    "https://olla.isai.kr"
   ]
 }
+
